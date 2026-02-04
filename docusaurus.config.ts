1e28b5f9bf5314969906f58c2d2c5a5de38f4729
Aye Zin Htun <ayezinhtun9@gmail.com>
2026-02-04 11:21:31 +0630

    update in best practise

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -66,7 +66,7 @@ const config: Config = {
       logo: { alt: '1CNG Logo', src: 'img/1cng.png' },
       items: [
         { to: '/docs/Getting Started/Introduction', label: 'User Guide', position: 'left' },
-        { to: '/docs/Best Practice', label: 'Best Practice', position: 'left'}, 
+        { to: '/', label: 'Best Practice', position: 'left'}, 
         { type: 'search', position: 'right' }, // 👈 search bar in navbar
       ],
     },
